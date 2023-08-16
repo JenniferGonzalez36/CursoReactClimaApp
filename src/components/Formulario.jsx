fc10520819753faa7d5f45d7a3d82c49bb15e85c
Jennifer González <jenniferjacqueline936@gmail.com>
2023-08-15 21:01:27 -0600

    Consuming the API with axios to get the Weather. Nothing is showed yet in the DOM.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import useClima from '../hooks/useClima';
 
 const Formulario = () => {
--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react';
+import axios from 'axios';
 
 const ClimaContext = createContext();
 
@@ -8,6 +9,7 @@ const ClimaProvider = ({ children }) => {
         ciudad: "",
         pais: ""
     });
+    const [resultado, setResultado] = useState({});
 
     const datosBusqueda = e => {
         setBusqueda({
@@ -16,8 +18,22 @@ const ClimaProvider = ({ children }) => {
         })
     };
 
-    const consultarClima = datos => {
-        console.log(datos);
+    const consultarClima = async datos => {
+        try{
+            const { ciudad, pais } = datos;
+            const appId = import.meta.env.VITE_API_KEY;
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+
+            const { data } = await axios(url);
+            const { lat, lon } = data[0];
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+            const { data: clima } = await axios(urlClima);
+            setResultado(clima);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
   return (
@@ -25,7 +41,8 @@ const ClimaProvider = ({ children }) => {
         value={{
             busqueda,
             datosBusqueda,
-            consultarClima
+            consultarClima,
+            resultado
         }}
     >
         { children }
